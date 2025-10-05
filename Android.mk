cc78443671fdcaa6e2387c1714abf0fd12af4671
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-10-05 18:16:41 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,7 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := SetAppops
-LOCAL_SRC_FILES := SetAppops.c
-LOCAL_CFLAGS := -Wall -Wextra -Werror -O2 -std=c99
+LOCAL_MODULE := enable_apk_install
+LOCAL_SRC_FILES := test.c
+LOCAL_CFLAGS := -Wall -Werror
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_64_BIT_ONLY := true
 include $(BUILD_EXECUTABLE)
