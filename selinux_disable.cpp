248905cd54fb7bd235b2f1be90587d86a2b16885
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-03-18 08:47:56 +0900

    Update selinux_disable.cpp

--- selinux_disable.cpp
+++ selinux_disable.cpp
@@ -1,70 +1,38 @@
-#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
 
-static void klog(const char *msg) {
-    int fd = open("/dev/kmsg", O_WRONLY | O_CLOEXEC);
-    if (fd >= 0) {
-        dprintf(fd, "selinux_disable: %s\n", msg);
-        close(fd);
-    }
-}
 static int is_enforcing(void) {
     int fd = open("/sys/fs/selinux/enforce", O_RDONLY | O_CLOEXEC);
-    if (fd < 0) {
-        klog("enforce failed");
-        return -1;
-    }
+    if (fd < 0) return -1;
 
     char buf[2] = {0};
     if (read(fd, buf, 1) != 1) {
         close(fd);
-        klog("enforce failed");
         return -1;
     }
     close(fd);
-
     return (buf[0] == '1') ? 1 : 0;
 }
+
 static int disable_selinux(void) {
-    const char *path = "/sys/fs/selinux/enforce";
-    int fd = open(path, O_WRONLY | O_CLOEXEC);
-    if (fd < 0) {
-        klog("enforce failed");
-        return -1;
-    }
+    int fd = open("/sys/fs/selinux/enforce", O_WRONLY | O_CLOEXEC);
+    if (fd < 0) return -1;
+
     if (write(fd, "0", 1) != 1) {
         close(fd);
-        klog("0);
         return -1;
     }
     close(fd);
-
-    klog("SELinux Permissive");
     return 0;
 }
 
 int main(void) {
-    klog("SELinux Permissive2");
     if (is_enforcing() == 1) {
         disable_selinux();
-    } else {
-        klog("Permissive ok");
     }
-
     while (1) {
         if (is_enforcing() == 1) {
-            klog("Enforcing");
             disable_selinux();
-        } else {
-            static int once = 0;
-            if (once == 0) {
-                klog("Permissive");
-                once = 1;
-            }
         }
         sleep(15);
     }
