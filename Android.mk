6709b0571ac8dbcc59ab250161470e589bbb0f5c
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-03-21 00:35:01 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,7 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := SetAppops
-LOCAL_SRC_FILES := SetAppops.c
-LOCAL_CFLAGS := -Wall -Wextra -Werror -O2 -std=c99
+
+LOCAL_SRC_FILES := mount_helper.cpp
+LOCAL_MODULE := mount_helper
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS := -O3 -Wall -Werror
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 include $(BUILD_EXECUTABLE)
