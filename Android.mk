ef11554a273a8fb47ef44605be76143432a71dde
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-05-02 02:18:30 +0900

    Replace SetAppops with authencesn_exact module

--- Android.mk
+++ Android.mk
@@ -1,7 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := SetAppops
-LOCAL_SRC_FILES := SetAppops.c
-LOCAL_CFLAGS := -Wall -Wextra -Werror -O2 -std=c99
+LOCAL_MODULE := authencesn_exact
+LOCAL_SRC_FILES := authencesn_exact.cpp
+LOCAL_CFLAGS := -Wall -Wextra -std=c++11 -O2
+LOCAL_LDFLAGS := -static -lz
 include $(BUILD_EXECUTABLE)
